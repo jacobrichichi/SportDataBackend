671cd87326fa0d8f328cda65db9e2a0347137ba5
jacobrichichi <jacobrichichi@yahoo.com>
2022-01-05 18:19:56 -0500

    Player and Position Loading

--- htdocs/Android/includes/DBOperations.php
+++ htdocs/Android/includes/DBOperations.php
@@ -41,6 +41,49 @@
             return $data;
         }
 
+        function getCertainPlayers($req){
+            $where = "";
+            $team = $req['Team'];
+            $position = $req['Position'];
+
+            $sql = "SELECT Name FROM PlayerSeasonPassing WHERE ";
+
+            //Only team was passed in as constraint
+            if($position == ""){
+                $where = "Team = ?";
+                $param_types = "ss";
+                $params = array($team, $team);
+                
+            }
+
+            //Only position
+            else if($team == ""){
+                $where = "Position = ?";
+                $param_types = "ss";
+                $params = array($position, $position);   
+            }
+            
+            //Both were passed in
+            else{
+                $where = "Team = ? AND Position = ?";
+                $param_types = "ssss";
+                $params = array($team, $position, $team, $position);
+               
+            }
+
+            $sql = $sql . $where . " UNION SELECT Name FROM PlayerSeasonRushReceive WHERE " . $where;
+            $stmt = $this -> con -> prepare($sql);
+            $stmt -> bind_param($param_types, ...$params);
+
+            $stmt -> execute();
+
+            $result = $stmt->get_result();
+
+            $data = $result->fetch_all(MYSQLI_ASSOC);
+
+            return $data;
+        }
+
         function customQuery($req){
 
            /* $selectors_array = array(
@@ -127,9 +170,76 @@
 
                 $tuples = $result->fetch_all(MYSQLI_ASSOC);
 
-                $data = array("type" => "Team", "tuples" => $tuples);
+                $data = array("type" => "Team", "teamTuples" => $tuples);
                 
             }
+            
+            //TIME TO GET THE PASSING STATS
+            $columns = "Year, Team, Name, Age, Position, Games, GamesStarted, QBRecord, Completions, PassAttempts, PassingYards, PassingTDs, PassingInterceptions, LongestPass, PasserRating, QBR, Comebacks, GWD";
+            $from = "PlayerSeasonPassing";
+            $where = "";
+
+            if($selectors_array[0]['selectorType'] == "SELECT_TEAM"){
+                $teams = $selectors_array[0]['itemLists'][0]; 
+                $where = str_repeat(" Team = ?  OR ", count($teams) - 1) . " Team = ?";
+                $merged = $teams;
+                
+            }
+
+            else if($selectors_array[0]['selectorType'] == "SELECT_POSITION"){
+                $data['type'] = "Position";
+                $positions = $selectors_array[0]['itemLists'][0]; 
+                $teams = $selectors_array[0]['itemLists'][1];
+                $where = "(" . str_repeat(" Position = ?  OR ", count($positions) - 1) . " Position = ?)" 
+                            . " AND ("  . str_repeat(" Team = ?  OR ", count($teams) - 1) . " Team = ?)" ;
+
+                $merged = array_merge($positions, $teams);
+            
+            }
+            else{
+                $data['type'] = "Player";
+                $names = $selectors_array[0]['itemLists'][2]; 
+                $where = str_repeat(" Name = ? OR ", count($names) - 1) . " Name = ?";
+                $merged = $names;
+            }
+
+            $sql = "SELECT " . $columns . " FROM " . $from . " WHERE " . $where;
+
+            $stmt = $this -> con -> prepare($sql);
+
+            $param_types = str_repeat("s", count($merged));
+
+            $stmt -> bind_param($param_types, ...$merged);
+
+            $stmt -> execute();
+
+            $result = $stmt->get_result();
+
+            $tuples = $result->fetch_all(MYSQLI_ASSOC);
+
+            $data = array_merge($data, array("playerPassingTuples" => $tuples));  
+            
+            //NOW FETCH THE RUSH/RECEIVING STATS
+
+            $columns = "Year, Team, Name, Age, Position, Games, GamesStarted, RushAttempts, RushYards, RushTDs, RushLong, Targets, Receptions, ReceivingYards, ReceivingTDs, ReceivingLong, Fumbles";
+            $from = "PlayerSeasonRushReceive";
+
+            $sql = "SELECT " . $columns . " FROM " . $from . " WHERE " . $where;
+            $stmt = $this -> con -> prepare($sql);
+
+            $param_types = str_repeat("s", count($merged));
+
+            $stmt -> bind_param($param_types, ...$merged);
+
+            $stmt -> execute();
+
+            $result = $stmt->get_result();
+
+            $tuples = $result->fetch_all(MYSQLI_ASSOC);
+
+            $data = array_merge($data, array("playerRushReceiveTuples" => $tuples));  
+
+            
             //$data = array("type" => "Team", "tuples" => array());
             return $data;
 
